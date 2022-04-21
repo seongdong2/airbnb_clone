ed6a0a1f9e1c06382bfd108d6f77fa9610328579
Dawn <chosd1123@naver.com>
2022-04-21 18:37:21 +0900

    #Extending Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        "25vh": "25vh",
+        "35vh": "35vh",
+        "50vh": "50vh",
+        "75vh": "75vh",
+        "110vh": "110vh",
+      },
+      colors: {
+
+        "rose-500": "#f43f5e",
+        "stone-100": "#f5f5f4",
+      }
+
+    },
   },
   plugins: [],
 }
